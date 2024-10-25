0efe707875731ea93700c3d517b0834889dfb3df
Anurag Bansal <devanuragbansal@gmail.com>
2024-10-25 07:30:01 +0530

    minor changes

--- src/Components/AddDocForm.jsx
+++ src/Components/AddDocForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { addDoc } from '../ReduxSlices/DocsSlice';
+import { addDoc } from '../ReduxSlices/DocsSlice.js';
 
 function AddDocForm({ onClose }) {
     const dispatch = useDispatch();
